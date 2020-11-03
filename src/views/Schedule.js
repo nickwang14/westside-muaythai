8a1f098f0c7fe83a8146ac78e5faaf28e67a17d9
Nick Wang <nick.wang@youi.tv>
2020-11-03 15:27:22 -0500

    Adding Embeded Acquity scheduler

--- src/views/Schedule.js
+++ src/views/Schedule.js
@@ -17,6 +17,7 @@ export default ({ fields }) => {
       <div className='section'>
         <div className='container'>
           <Content source={body} />
+          <iframe src="https://app.acuityscheduling.com/schedule.php?owner=20144425" title="Schedule Appointment" width="100%" height="800" frameBorder="0"></iframe><script src="https://embed.acuityscheduling.com/js/embed.js" type="text/javascript"></script>
         </div>
       </div>
     </main>
