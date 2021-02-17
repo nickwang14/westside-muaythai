7c5f4cc9a6a9c9cb96b4417ee2f99e7345c58b2b
nickwang14 <nickwang14@gmail.com>
2021-02-17 15:58:19 -0500

    Updating footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ import './Footer.css'
 
 export default ({ globalSettings, socialSettings, navLinks }) => (
   <footer className='Footer'>
-    <div className='container taCenter'>
-      <span>© 2020 All rights reserved.</span>
-    </div>
+      <div className='container taCenter'>
+        <span>© 2021 All rights reserved.</span>
+      </div>
   </footer>
 )
