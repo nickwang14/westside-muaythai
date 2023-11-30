a50fe2e6b034e9298709c87e69e311417ff02fa8
nickwang14 <nickwang14@gmail.com>
2023-11-30 08:30:00 -0500

    fix

--- src/views/Schedule.js
+++ src/views/Schedule.js
@@ -1,12 +1,19 @@
 import React from 'react'
-
 import Content from '../components/Content'
+import PageHeader from '../components/PageHeader'
 import './Schedule.css'
 
 export default ({ fields }) => {
-  const { body } = fields
+  const { title, subtitle, featuredImage, body } = fields
   return (
-    <main className='Schedule'>
+    <main className='Home'>
+     <PageHeader
+        large
+        title={title}
+        subtitle={subtitle}
+        backgroundImage={featuredImage}
+      />
+
       <div className='section'>
         <div className='container'>
           <Content source={body} />
