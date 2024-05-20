2e93ea70405dcd1185b0d8282d56821a62fd663a
nickwang14 <nickwang14@gmail.com>
2024-05-20 15:58:14 -0400

    Fix Social Size, Remove reviews

--- src/views/Home.js
+++ src/views/Home.js
@@ -21,33 +21,17 @@ export default ({ fields }) => {
          
         </div>
       </div>
-      <div className='section'>
-      <script src="https://embedsocial.com/js/iframe.js"></script>
-        <iframe 
-          src="https://embedsocial.com/api/pro_hashtag/be3f9795e621c7690977a937eaf52c594cb46f70" 
-          width="900px"
-          height="1200px" 
-          frameborder="0" 
-          marginheight="0" 
-          marginwidth="0"
-          title="Review Feed"
-          >
-          </iframe>
-      </div>
-      <div className='section'>
         <script src="https://embedsocial.com/js/iframe.js"></script>
           <iframe style=
             {{
               border: 0, 
               width: '100%', 
-              height: '100%'
+              height: '550px'
             }} 
             scrolling="no" src="https://embedsocial.com/api/pro_hashtag/1dd5e27fe7b860079e795ac58fe52b8d9aa2b7a3" title="Instagram Feed"
             >
           </iframe>
         <script>iFrameResize()</script>
-      </div>
-      
     </main>
   )
 }
